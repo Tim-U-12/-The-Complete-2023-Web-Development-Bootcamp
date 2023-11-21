d46f5de558eaa43c77d92152fa25c18a83939224
Tim <tim.a.ung.12@gmail.com>
2023-11-21 14:06:09 +1100

    created drumkit

--- Lessons/17. Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/dicee.js
+++ Lessons/17. Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/dicee.js
@@ -2,17 +2,19 @@ function rolldie(sides) {
     return Math.floor(Math.random() * sides) + 1
 }
 
+function setDie(){
+    var die1 = rolldie(6)
+    var die2 = rolldie(6)
+    document.querySelector(".img1").setAttribute("src", "./images/dice" + die1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "./images/dice" + die2 + ".png");
 
-var die1 = rolldie(6)
-var die2 = rolldie(6)
-document.querySelector(".img1").setAttribute("src", "./images/dice" + die1 + ".png");
-document.querySelector(".img2").setAttribute("src", "./images/dice" + die2 + ".png");
-
-if (die1 > die2) {
-    document.querySelector(".container h1").textContent = "Player 1 Wins!"
-} else if (die1 < die2) {
-    document.querySelector(".container h1").textContent = "Player 2 Wins!"
-} else {
-    document.querySelector(".container h1").textContent = "Draw!"
+    if (die1 > die2) {
+        document.querySelector(".container h1").textContent = "🚩Player 1 Wins!"
+    } else if (die1 < die2) {
+        document.querySelector(".container h1").textContent = "Player 2 Wins!🚩"
+    } else {
+        document.querySelector(".container h1").textContent = "Draw!"
+    }
 }
 
+setDie();
