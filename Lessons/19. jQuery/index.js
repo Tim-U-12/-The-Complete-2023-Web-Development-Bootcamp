e209e9b5815f0213c4be6cf25ca7b390a75e755e
Tim <tim.a.ung.12@gmail.com>
2023-11-25 12:28:40 +1100

    using eventlisteners to add attributes

--- Lessons/19. jQuery/index.js
+++ Lessons/19. jQuery/index.js
@@ -1,5 +1,22 @@
 // when you want the script tag in the header
 $(document).ready(function() {
-    $("h1").addClass("big-title margin-50");
+    $("h1").addClass("big-title margin-50 ");
+    $("a").attr("href", "https://au.yahoo.com/");
+    $("h1").click(function() {
+        $("h1").toggleClass("blue-background");
+        console.log($("h1").attr("class"));
+    });
+
+    $("h1").on("mouseover", function(){
+        $("h1").css("color", "pink")
+    });
+
+    $(".hide-title").click(function() {
+        $("h1").toggle();
+    });
+
+    $(document).keypress(function(event){
+        $("h1").text(event.key);
+    });
 });
 
