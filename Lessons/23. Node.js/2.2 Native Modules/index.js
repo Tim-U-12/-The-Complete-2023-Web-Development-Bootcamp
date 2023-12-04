facacec8317006e55bcbc1980cdaf422250705b1
Tim <tim.a.ung.12@gmail.com>
2023-12-04 14:09:36 +1100

    learned how to do simple read and write using filesystem's writefile and readfile

--- Lessons/23. Node.js/2.2 Native Modules/index.js
+++ Lessons/23. Node.js/2.2 Native Modules/index.js
@@ -1,6 +1,12 @@
 const fs = require("fs")
 
-fs.writeFile("myMessage.txt", "Hello from Node.js", (err) =>{
+// fs.writeFile("myMessage.txt", "Hello from Node.js", (err) =>{
+//     if (err) throw err;
+//     console.log("thie file has been saved")
+// });
+
+const filePath = "./myMessage.txt"
+fs.readFile(filePath, 'utf-8',(err, data) => {
     if (err) throw err;
-    console.log("thie file has been saved")
+    console.log(data)
 });
