d4b0a671b64ae6934294413018865674a2af2f72
Tim <tim.a.ung.12@gmail.com>
2023-12-04 14:41:16 +1100

    Learnt how to use simple NPM packages

---
+++ Lessons/23. Node.js/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+import generateName from "sillyName";
+import superheroes from "superheroes"
+
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}.`);
+
+var superHero = superheroes.random();
+console.log(`Here comes the ${superHero}!`)
