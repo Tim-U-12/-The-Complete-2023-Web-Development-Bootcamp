8966518e31d815e3bc233ea817ff8ce8baf589a8
Tim <tim.a.ung.12@gmail.com>
2023-12-11 15:23:51 +1100

    intro to creating custom middleware

--- Lessons/24. Express.js with Node.js/3.4 Middleware/index1.js
+++ Lessons/24. Express.js with Node.js/3.4 Middleware/index1.js
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
+import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -8,6 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
