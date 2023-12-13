c09a2d9014958b23a84fb957770233dd0b25ae58
Tim <tim.a.ung.12@gmail.com>
2023-12-14 10:44:32 +1100

    use ejs to create a simple dynamic webpage that returns the number of letters from a name inputted

--- Lessons/25. EJS/4.2 Passing Data/index.js
+++ Lessons/25. EJS/4.2 Passing Data/index.js
@@ -6,9 +6,19 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+const data = {
+  title: "Enter your name below"
+}
 
-app.post("/submit", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs", data)
+});
+
+app.post("/submit", (req, res) => {
+  let lenName = req.body['fName'].length + req.body['lName'].length
+  data['title'] = `There are ${lenName} letters in your name.`
+  res.render("index.ejs", data)
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
