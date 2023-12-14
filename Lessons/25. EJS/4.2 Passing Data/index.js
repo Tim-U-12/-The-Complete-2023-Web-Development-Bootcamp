8eef55f85348ee746c8536cbbf13734dfcf1e549
Tim <tim.a.ung.12@gmail.com>
2023-12-14 11:23:38 +1100

    changed the code to be more efficient

--- Lessons/25. EJS/4.2 Passing Data/index.js
+++ Lessons/25. EJS/4.2 Passing Data/index.js
@@ -6,18 +6,13 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const data = {
-  title: "Enter your name below"
-}
-
 app.get("/", (req, res) => {
-  res.render("index.ejs", data)
+  res.render("index.ejs")
 });
 
 app.post("/submit", (req, res) => {
   let lenName = req.body['fName'].length + req.body['lName'].length
-  data['title'] = `There are ${lenName} letters in your name.`
-  res.render("index.ejs", data)
+  res.render("index.ejs", {lenName : lenName})
 });
 
 app.listen(port, () => {
