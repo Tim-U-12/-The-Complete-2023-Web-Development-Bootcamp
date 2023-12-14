05ce64997bf9f7acc241b1f14c1e6037b42e5890
Tim <tim.a.ung.12@gmail.com>
2023-12-14 12:43:13 +1100

    Created a webpage that would randomly generate band names

--- Lessons/25. EJS/4.4 Band Generator Project/index.js
+++ Lessons/25. EJS/4.4 Band Generator Project/index.js
@@ -8,6 +8,7 @@ const port = 3000;
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
+app.use(express.static("public"))
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
@@ -16,11 +17,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs")
 });
 
 app.post("/submit", (req, res) => {
   //Step 2 - Make the generate name functionality work
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
+  let randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  res.render("index.ejs", {randomAdj : randomAdj, randomNoun : randomNoun})
   //Then:
   //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
   //scroll down to see the two arrays.
